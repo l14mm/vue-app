46a8b30adb54d565d0d47b252584f2b9302905a8
liammoroney <liam.moroney14@gmail.com>
2018-10-03 20:13:40 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <SearchBar @termChange="onTermChange"></SearchBar>
+  </div>
+</template>
+
+<script>
+import SearchBar from './components/SearchBar.vue'
+import { API_KEY } from "./keys.json"
+
+export default {
+  name: 'app',
+  components: {
+    SearchBar
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      console.log(searchTerm)
+      console.log(API_KEY)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
