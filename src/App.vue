94f814b0567caa32193e7fbcd4f8b4f5927e7d4f
liammoroney <liam.moroney14@gmail.com>
2018-10-05 21:20:09 +0100

    Added youtube api query to return search term. Display each video thumbnail and title in a list in child components. Added bootstrap.

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,38 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList :videos="videos"></VideoList>
+    {{ videos.length }}
   </div>
 </template>
 
 <script>
-import SearchBar from './components/SearchBar.vue'
-import { API_KEY } from "./keys.json"
+import axios from "axios";
+import SearchBar from "./components/SearchBar.vue";
+import VideoList from "./components/VideoList.vue";
+import { API_KEY } from "./keys.json";
 
 export default {
   name: 'app',
   components: {
-    SearchBar
+    SearchBar,
+    VideoList
+  },
+  data() {
+    return { videos: [] };
   },
   methods: {
     onTermChange(searchTerm) {
-      console.log(searchTerm)
-      console.log(API_KEY)
+      axios.get("https://www.googleapis.com/youtube/v3/search", {
+        params: {
+          key: API_KEY,
+          type: "vidio",
+          part: "snippet",
+          q: searchTerm
+        }
+      }).then(response => {
+        this.videos = response.data.items;
+      });
     }
   }
 }
---
+++ src/components/VideoList.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul class="list-group">
+        <VideoListItem 
+        v-for="video in videos"
+        :video="video"
+        :key="video.etag"
+        >
+        </VideoListItem>
+    </ul>
+</template>
+
+<script>
+import VideoListItem from "./VideoListItem";
+
+export default {
+    name: "VideoList",
+    props: ["videos"],
+    components: {
+        VideoListItem
+    }
+};
+</script>
+
+<style>
+
+</style>
+
