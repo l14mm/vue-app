274318b10bae3801e8439b80977c27bd00eae091
liammoroney <liam.moroney14@gmail.com>
2018-10-21 20:39:23 +0100

    Added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <SearchBar @termChange="onTermChange"></SearchBar>
-    <div class="row">
-      <VideoDetail :video="selectedVideo"></VideoDetail>
-      <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">My App</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Search Youtube</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Other
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    <div id="main">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <SearchBar @termChange="onTermChange"></SearchBar>
+      <div class="row">
+        <VideoDetail :video="selectedVideo"></VideoDetail>
+        <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+      </div>
     </div>
   </div>
 </template>
@@ -49,7 +82,7 @@ export default {
 </script>
 
 <style>
-#app {
+#main {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
