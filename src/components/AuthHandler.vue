2563aa5bec19571cd8f24fb2aa54f2c22ce1973e
liammoroney <liam.moroney14@gmail.com>
2018-10-23 17:38:15 +0100

    Added vue router and a route for the oauth2 callback.

---
+++ src/components/AuthHandler.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Please wait...
+    </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+    name: "AuthHandler",
+    methods: mapActions(["finaliseLogin"]),
+    created() {
+        this.finaliseLogin(window.location.hash)
+    }
+}
+</script>
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,5 @@
-import api from "../../api/imgur"
+import api from "../../api/imgur";
+import qs from "qs";
 
 const state = {
     token: null
@@ -12,6 +13,11 @@ const actions = {
     login: () => {
         api.login();
         console.log('login')
+    },
+    finaliseLogin: ({ commit }, hash) => {
+        const query = qs.parse(hash.replace("#", ""));
+
+        commit("setToken", query.access_token);
     },
     logout: ({ commit }) => {
         commit("setToken", null);
